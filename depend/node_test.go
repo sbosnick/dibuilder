2989089c19757a575108e7204e5b0b90fb56c207
Steven Bosnick <sbosnick@sympatico.ca>
2017-02-12 18:24:20 -0500

    Removed empty file.

--- depend/node_test.go
+++
@@ -1,5 +0,0 @@
-// Copyright Steven Bosnick 2017. All rights reserved.
-// Use of this source code is governed by the GNU General Public License version 3.
-// See the file COPYING for your rights under that license.
-
-package depend
